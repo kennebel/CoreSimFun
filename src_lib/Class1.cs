f8358bd562195114440f4c48361e1c8cf3a2b47b
kennebel <jac@kennebel.com>
2017-08-12 22:19:00 -0600

    Initial check in

---
+++ src_lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace src_lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ src_sim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace src_sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There~");
+        }
+    }
+}
